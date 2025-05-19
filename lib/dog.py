dfb444da4aa32e35a080c7f7005b3d5bfd86a036
Mercy <owinomercy983@gmail.com>
2025-05-19 14:57:00 +0300

    attributes

--- lib/dog.py
+++ lib/dog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# List of approved breeds
 APPROVED_BREEDS = [
     "Mastiff",
     "Chihuahua",
@@ -12,4 +13,39 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Unknown"):
+        self._name = None
+        self._breed = None
+        self.name = name  # Using the setter for name
+        if self._name:  # Only proceed to breed if name is valid
+            self.breed = breed  # Using the setter for breed
+
+    # Name property
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) == 0 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    # Breed property
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, value):
+        if value not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = value
+
+    def sit(self):
+        print(f"{self._name} is sitting.")
+
+    def get_breed(self):
+        return self._breed
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,38 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unknown"):
+        self._name = None
+        self._job = None
+        self.name = name  # Using the setter for name
+        self.job = job  # Using the setter for job
+
+    # Name property
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) == 0 or len(value) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()  # Convert to title case before saving
+
+    # Job property
+    @property
+    def job(self):
+        return self._job
+    
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
+
+    def get_name(self):
+        return self._name
+
+    def get_job(self):
+        return self._job
